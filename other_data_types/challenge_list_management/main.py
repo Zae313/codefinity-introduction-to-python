6338643453212719fcd9bee7a588c474ca9d5583
Zae313 <>
2026-02-14 06:03:34 +0000

    Challenge: List Management

---
+++ other_data_types/challenge_list_management/main.py
@@ -0,0 +1,6 @@
+meat = ["Ham", 3.99, 50, "Sliced"]
+cheese = ["Cheddar", 5.49, 100, "Sharp"]
+condiment = ["Mustard", 1.99, 75, "Spicy"]
+deli_dept = [meat, cheese, condiment]
+if meat[2] < 100:
+    
